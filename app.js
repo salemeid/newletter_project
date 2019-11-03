6ad61fce2ebbfc6467b5704d848c2f75255148ce
Salem Eid <salem.eid@gmail.com>
2019-11-03 21:49:58 +0300

    first commit version 1

---
+++ app.js
@@ -0,0 +1,81 @@
+//jshint esversion: 6
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+//css and images needs to be in public folder and using below code
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extedned: true}));
+
+app.get("/",function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/",function(req, res){
+  //console.log("hello");
+  var  firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+    url: "https://us5.api.mailchimp.com/3.0/lists/699d62adc5",
+    method: "POST",
+    headers: {
+      "Authorization" : "salem1 1c63c74f7a5b0fd685be3b3f9bb966a7-us5"
+    },
+    body: jsonData
+
+  };
+
+  request(options, function(error, response, body){
+    if (error) {
+      res.sendFile(__dirname + "/failure.html");
+    } else {
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+      }
+
+    }
+
+  });
+});
+//try again section to return to homepage.
+app.post("/failure", function(req, res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000,function(){
+  console.log("server started listening on port 3000");
+});
+
+
+
+
+//API Key
+//1c63c74f7a5b0fd685be3b3f9bb966a7-us5
+
+
+//list // ID
+// 699d62adc5
